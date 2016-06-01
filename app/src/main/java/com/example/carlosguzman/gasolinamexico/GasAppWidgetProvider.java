f30b26baa8ac67d6ba6a6d2af9460df0d6a5693f
Carlos Guzman <carlosaguzman@gmail.com>
2016-06-01 18:44:53 -0500

    Parcialmente anadiendo widget configuration

--- app/src/main/java/com/example/carlosguzman/gasolinamexico/GasAppWidgetProvider.java
+++ app/src/main/java/com/example/carlosguzman/gasolinamexico/GasAppWidgetProvider.java
@@ -7,6 +7,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * Created by 501820531 on 5/27/2016.
  */
@@ -21,11 +24,17 @@ public class GasAppWidgetProvider extends AppWidgetProvider {
 
             Intent intent = new Intent(context, MainActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
+            Calendar c = Calendar.getInstance();
+            SimpleDateFormat df = new SimpleDateFormat("dd/mmm/yyyy");
+            String formattedDate = df.format(c.getTime());
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.activity_widget);
             views.setOnClickPendingIntent(R.id.wid_gasolina, pendingIntent);
             views.setOnClickPendingIntent(R.id.wid_precio, pendingIntent);
+            views.setTextViewText(R.id.wid_gasolina, "my test Gas");
+            views.setTextViewText(R.id.wid_precio,"0.00");
+            views.setTextViewText(R.id.wid_fecha,formattedDate);
+
 
 
             // Tell the AppWidgetManager to perform an update on the current app widget
---
+++ app/src/main/java/com/example/carlosguzman/gasolinamexico/confWidget.java
@@ -0,0 +1,6 @@
+package com.example.carlosguzman.gasolinamexico;
+
+import android.app.Activity;
+
+public class confWidget extends Activity{
+}
